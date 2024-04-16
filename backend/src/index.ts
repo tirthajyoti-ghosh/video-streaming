df2a2f17cf8982857d8b24b45230231f89c8613f
Tirthajyoti Ghosh <itirthahere@gmail.com>
2024-04-16 15:51:47 +0530

    [BACKEND] add(media-list): implement media list api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config({ path: '.env' });
 
@@ -9,8 +11,32 @@ app.use(express.json());
 
 const port = 3000;
 
-app.get('/', async (req, res) => {
-  res.send('Hello World!');
+async function readDirectory(dir: string): Promise<any> {
+  const result: any = {};
+
+  const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
+
+  for (const dirent of dirents) {
+    const filePath = path.join(dir, dirent.name);
+
+    if (dirent.isDirectory()) {
+      const nestedFiles = await readDirectory(filePath);
+      result[dirent.name] = nestedFiles[dirent.name] || nestedFiles;
+    } else if (dirent.isFile() && !dirent.name.endsWith('.parts')) {
+      if (!result['files']) {
+        result['files'] = [];
+      }
+      result['files'].push(dirent.name);
+    }
+  }
+
+  return result;
+}
+
+app.get('/media/list', async (req, res) => {
+    const mediaPath = process.env.MEDIA_PATH as string;
+    const media = await readDirectory(mediaPath);
+    res.json(media);
 });
 
 app.listen(port, () => {
